6c2ae694d46ec081ab975e7806d6f79e86d4898d
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-10-14 22:38:38 +0530

    Add. Security filter chain configuration

--- src/main/java/com/springsecuritybasics/security/SecurityConfig.java
+++ src/main/java/com/springsecuritybasics/security/SecurityConfig.java
@@ -2,24 +2,37 @@ package com.springsecuritybasics.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf ->
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcRequestMatcher = new MvcRequestMatcher.Builder(introspector);
+
+        httpSecurity.headers(header ->
+                header.frameOptions(frameOptionsConfig ->
+                        frameOptionsConfig.sameOrigin()
+                )
+        ).csrf(csrf ->
                 csrf.disable()
         ).sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         ).authorizeHttpRequests(authorize ->
-                authorize.anyRequest().permitAll()
-        );
+                authorize.requestMatchers(antMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(mvcRequestMatcher.pattern("/signup")).permitAll()
+                        .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
--- src/test/java/com/springsecuritybasics/controllers/UserControllerTest.java
+++ src/test/java/com/springsecuritybasics/controllers/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -48,6 +49,11 @@ public class UserControllerTest {
                 MockMvcRequestBuilders
                         .get("/authenticated")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors
+                                .user(signupRequest.username())
+                                .password(signupRequest.password())
+                                .roles(signupRequest.role())
+                        )
         );
 
         result.andExpect(
