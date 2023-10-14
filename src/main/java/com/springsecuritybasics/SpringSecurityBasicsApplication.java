2525fe732cc8058952586b1938adf1695c210491
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-10-14 22:08:51 +0530

    Initial commit

---
+++ src/main/java/com/springsecuritybasics/SpringSecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecuritybasics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
